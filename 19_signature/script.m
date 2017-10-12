0f8a9a6fdbb3ac3e6f40f95625551dae7b289356
Iván Munguía <xxthecreatorxx_live@hotmail.com>
2017-10-11 20:20:18 -0500

    Add example of the generation of the signature of an image

---
+++ 19_signature/script.m
@@ -0,0 +1,31 @@
+clc, clear all, close all;
+
+image = rgb2gray(imread('image.bmp'));
+edge_image = edge(image);
+
+[rows, cols] = size(image);
+pixel = 0;
+for i = 1:rows
+	for j = 1:cols
+		if edge_image(i, j) == 1
+			pixel = pixel + 1;
+			row_counter(pixel) = i;
+			column_counter(pixel) = j;
+		end
+	end
+end
+
+sum_rows = sum(row_counter);
+sum_cols = sum(column_counter);
+x = round(sum_rows/pixel);
+y = round(sum_cols/pixel);
+
+edge_image(x-1:x+1, y-1:y+2) = 1;
+
+figure, imshow(edge_image)
+
+for i = 1:pixel
+	firm(i) = sqrt((x - row_counter(i))^2 + (y - column_counter(i))^2);
+end
+
+figure, plot(firm)
